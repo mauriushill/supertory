c33194d1db5eca29b33e8e82cc965c85b409c0a4
Maurius Hill <146677371+mauriushill@users.noreply.github.com>
2024-03-29 21:41:16 +0000

    Generated comments from scaffold.

---
+++ app/models/comment.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  item_id    :integer          not null
+#  user_id    :integer          not null
+#
+# Indexes
+#
+#  index_comments_on_item_id  (item_id)
+#  index_comments_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  item_id  (item_id => items.id)
+#  user_id  (user_id => users.id)
+#
+class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :item
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_29_213757) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_29_214019) do
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id", null: false
+    t.integer "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_comments_on_item_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "facilities", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -69,6 +79,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_29_213757) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "items"
+  add_foreign_key "comments", "users"
   add_foreign_key "items", "facilities"
   add_foreign_key "items", "folders"
   add_foreign_key "items", "products"
