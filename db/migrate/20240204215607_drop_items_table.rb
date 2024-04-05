05a9578a689b18d78ebd1964fe8f17bcae49a42f
Maurius Hill <146677371+mauriushill@users.noreply.github.com>
2024-04-05 14:59:41 +0000

    migration

--- db/migrate/20240204215607_drop_items_table.rb
+++ db/migrate/20240204215607_drop_items_table.rb
@@ -1,11 +1,11 @@
 class DropItemsTable < ActiveRecord::Migration[7.0]
   def change
+    drop_table :comments if ActiveRecord::Base.connection.table_exists? :comments
+    drop_table :items if ActiveRecord::Base.connection.table_exists? :items
     drop_table :purchases if ActiveRecord::Base.connection.table_exists? :purchases
     drop_table :facilities if ActiveRecord::Base.connection.table_exists? :facilities
     drop_table :products if ActiveRecord::Base.connection.table_exists? :products
     drop_table :folders if ActiveRecord::Base.connection.table_exists? :folders
-    drop_table :comments if ActiveRecord::Base.connection.table_exists? :comments
-    drop_table :items if ActiveRecord::Base.connection.table_exists? :items
 
   end
 end
