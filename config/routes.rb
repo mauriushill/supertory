7e64805e7fb1c4e3b03c0b9765387f16e8027de9
CarlosCastanedaDev <carlos@nemin.dev>
2024-05-06 21:28:34 -0500

    code review feedback

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   root "welcome#index"
+  # Remove unused code
   # root "items#index"
-  
+
   devise_for :users
-  
+
   resources :comments
   resources :products
   resources :purchases
   resources :facilities
   resources :folders
   resources :items
-  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
